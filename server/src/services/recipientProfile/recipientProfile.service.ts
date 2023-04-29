ed95788a2036dd4aa1389f3289ca00a6987054b0
Ngọc Phú <ngocphupham682001@gmail.com>
2023-04-30 00:46:59 +0700

    Update recipientProfile.service.ts

--- server/src/services/recipientProfile/recipientProfile.service.ts
+++ server/src/services/recipientProfile/recipientProfile.service.ts
@@ -295,6 +295,8 @@ export default class RecipentProfileService {
             `Info User not match from ${infoUser.identity} at identity ${entityValidate.identity}`,
           );
         case infoUser.address !== entityValidate.placeOfBirth:
+          console.log(infoUser.address);
+          console.log(entityValidate.placeOfBirth);
           throw new BadRequestError(
             `Info User not match from ${infoUser.identity} at address ${entityValidate.placeOfBirth}`,
           );
