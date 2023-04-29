84e8ca6109bbf8cdc63f5626bb96293e173f6ef4
Ngọc Phú <ngocphupham682001@gmail.com>
2023-04-30 00:32:18 +0700

    Update schema.ts

--- server/src/services/CRUD/certificateType/schema.ts
+++ server/src/services/CRUD/certificateType/schema.ts
@@ -7,17 +7,7 @@ export default {
     type: Joi.string()
       .valid(...Object.values(Type))
       .required(),
-    level: Joi.when('type', {
-      is: 'DIPLOMA',
-      then: Joi.string(),
-      otherwise: Joi.number().integer().min(0).required(),
-    }).custom((value, helpers) => {
-      const dataReq = helpers.state.ancestors[0];
-      if (dataReq.type === 'DIPLOMA' && value !== null) {
-        return helpers.error('any.invalid');
-      }
-      return value;
-    }),
+    level: Joi.number().required().integer().min(0) ,
   }),
   edit : Joi.object().keys({
     id : Joi.string().required(),
@@ -25,17 +15,7 @@ export default {
     type: Joi.string()
       .valid(...Object.values(Type))
       .required(),
-    level: Joi.when('type', {
-      is: 'DIPLOMA',
-      then: Joi.string().allow(null),
-      otherwise: Joi.number().integer().min(1).required(),
-    }).custom((value, helpers) => {
-      const dataReq = helpers.state.ancestors[0];
-      if (dataReq.type === 'DIPLOMA' && value !== null) {
-        return helpers.error('any.invalid');
-      }
-      return value;
-    }),
+    level: Joi.number().required().integer().min(0),
   }),
   pagination: Joi.object().keys({
     page: Joi.number().required().integer().min(1),
