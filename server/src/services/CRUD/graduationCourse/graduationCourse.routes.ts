80b7643243f290d9d737f33060d80bd1db151074
Ngọc Phú <ngocphupham682001@gmail.com>
2023-05-01 18:25:10 +0700

    Update graduationCourse.routes.ts

--- server/src/services/CRUD/graduationCourse/graduationCourse.routes.ts
+++ server/src/services/CRUD/graduationCourse/graduationCourse.routes.ts
@@ -2,52 +2,48 @@ import { Router } from 'express';
 import { authentication, authorization } from '../../../shared/middlewares';
 import { Role } from '../../../shared/database/model';
 import { role } from '../../../shared/helpers/utils';
-import {
-  CertificateTypeRepository,
-  DACRepository,
-} from '../../../shared/database/repository';
-
 import validator, { ValidationSource } from '../../../shared/helpers/validator';
 import schema from './schema';
-import { Controller, Service } from '../base';
+import {
+  GraduationCourseRepository,
+  DACRepository,
+} from '../../../shared/database/repository';
+import { Service, Controller } from '../base';
 
 const router = Router();
-const certTypeRepository = new CertificateTypeRepository();
+const graduationCourseRepo = new GraduationCourseRepository();
 const dacRepository = new DACRepository();
-const certTypeService = new Service(certTypeRepository, dacRepository);
-const certTypeController = new Controller(certTypeService);
+const graduationService = new Service(graduationCourseRepo, dacRepository);
+const graduationCourseController = new Controller(graduationService);
 
+router.use(authentication, role(Role.UNIVERSITY), authorization);
 
 router.get(
   '/',
   validator(schema.pagination, ValidationSource.QUERY),
-  authentication, role(Role.DOET,Role.UNIVERSITY), authorization,
-  certTypeController.getList,
+  graduationCourseController.getList,
 );
+
 
 router.get(
   '/:id',
-  validator(schema.certTypeId, ValidationSource.PARAM),
-  authentication, role(Role.DOET), authorization,
-  certTypeController.detail,
+  validator(schema.courseTypeId, ValidationSource.PARAM),
+  graduationCourseController.detail,
 );
 
-router.post('/', validator(schema.create), certTypeController.create);
+router.post('/', validator(schema.create), graduationCourseController.create);
+
 
 router.put(
   '/:id',
-  validator(schema.certTypeId, ValidationSource.PARAM),
+  validator(schema.courseTypeId, ValidationSource.PARAM),
   validator(schema.edit),
-  authentication, role(Role.DOET), authorization,
-  certTypeController.edit,
+  graduationCourseController.edit,
 );
 
 router.delete(
   '/:id',
-  validator(schema.certTypeId, ValidationSource.PARAM),
-  authentication, role(Role.DOET), authorization,
-  certTypeController.delete,
+  validator(schema.courseTypeId, ValidationSource.PARAM),
+  graduationCourseController.delete,
 );
-
-
 export default router;
