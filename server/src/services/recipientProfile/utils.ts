d7d8e096049429e1b82b813526187ea4367a58fd
Ngọc Phú <ngocphupham682001@gmail.com>
2023-05-04 23:40:43 +0700

    Update utils.ts

--- server/src/services/recipientProfile/utils.ts
+++ server/src/services/recipientProfile/utils.ts
@@ -25,7 +25,8 @@ export const hasDuplicateAndMustDuplicateIU = (students: DAC[]): boolean => {
 
 
 export const filterConditionRecipientProfileAndIdNumber = async (flag: FlagFilter, listRecipientProfile: DAC[]) => {
-  const filteredList = listRecipientProfile.filter(el =>
+   console.log(listRecipientProfile);
+   const filteredList = listRecipientProfile.filter(el =>
     (flag.registrationNumber ? el.registrationNum !== null : el.registrationNum === null) &&
     (flag.idNumber ? el.idNumber !== null : el.idNumber === null)
   );
