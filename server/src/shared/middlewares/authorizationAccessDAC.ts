86c99bff108df24a86d942a8c7f7862f31808f12
ngocphuphamm <ngocphupham682001@gmail.com>
2023-06-21 04:08:23 +0700

    fix: detail

--- server/src/shared/middlewares/authorizationAccessDAC.ts
+++ server/src/shared/middlewares/authorizationAccessDAC.ts
@@ -1,18 +1,24 @@
+import { ADMIN_ID } from '../../common/constant';
 import { AuthFailureError, BadRequestError } from '../core/apiError';
 import { DACRepository } from '../database/repository';
+import { queryCertificateByUUID } from '../fabric/callFuncChainCode';
 import asyncHandler from '../helpers/asyncHandler';
 import { ProtectedRequest } from 'app-request';
 import { Types } from 'mongoose';
 
-const dacRepository = new DACRepository();
 export const authorizationAccessDAC = asyncHandler(
   async (req: ProtectedRequest, res, next) => {
     const { idDAC } = req.params;
     const { userName } = req.user;
-
-    const dac = await dacRepository.findById(new Types.ObjectId(idDAC));
-    if(!dac) throw new BadRequestError('DAC not exist');
-
+    let certBlockchain;
+    try{
+       certBlockchain = await queryCertificateByUUID(idDAC, ADMIN_ID);
+    
+    }
+    catch(err){
+      throw new BadRequestError('DAC not exist');
+    }
+    const dac = JSON.parse(certBlockchain.properties);
     if(dac.iSt !== userName)
     {
       throw new AuthFailureError('Authorization not access this dac');
